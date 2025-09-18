5b3d48d40f4c78223ca816328b2878b7f52b5c81
K-Canzater <moneymaking14223@gmail.com>
2025-09-18 17:38:44 -0400

    Fixed Typo

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,17 +1,16 @@
 from playwright.sync_api import Page
 
+
 class InventoryPage:
 
-    def __init__(self,page):
+    def __init__(self, page):
         self.page = page
         self.first_item_add_btn = "#add-to-cart-sauce-labs-backpack"
         self.cart_icon = ".shopping_cart_link"
 
-
     def add_first_item_to_cart(self):
         self.page.wait_for_selector(self.first_item_add_btn)
         self.page.click(self.first_item_add_btn)
-
 
     def click_shopping_cart(self):
         self.page.wait_for_selector(self.cart_icon)
